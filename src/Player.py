b8b9f7b80586676323e825c446278c26932f8903
Noh Da-Bin <scnoh02@naver.com>
2023-03-26 23:38:05 +0900

    feat: In-Game logic

--- src/Player.py
+++ src/Player.py
@@ -1,24 +1,54 @@
 class Player:
     def __init__(self, isComputer):
         self.isComputer = isComputer
+    def __init__(self, is_computer):
+        self.is_computer = is_computer
         self.current_card
         self.hand = []
         self.possible_cards = []
-    
+        self.is_authority
+        self.is_turn_used
+        self.is_uno = False
+
     def press_uno(self):
         pass
-    
+
+        if self.is_authority == True and len(self.hand):
+            self.is_uno = True
+
     def use_card(self, index):
         self.current_card = self.hand[index]
         self.hand.remove(index)
-    
+        self.is_turn_used = True
+
     def get_card():
         pass
+    def get_card(self):
+        GameManager.give_card(GameManager.turn)
+        self.is_turn_used = True
 
 class User(Player):
     def __init__(self, isComputer):
         self.isComputer = isComputer
-        
+    def __init__(self, is_computer):
+        self.is_computer = is_computer
+
 class Computer(Player):
     def __init__(self, isComputer):
         self.isComputer = isComputer
+    def __init__(self, is_computer):
+        self.is_computer = is_computer
+
+    def computer_play(self):
+        for i in range(len(self.hand)):
+            if GameManager.GraveTop.color == hand[i].color or GameManager.GraveTop.num == hand[i].num:
+                self.possible_cards.append[ self.hand[i] ]
+
+        if len(self.possible_cards) != 0:
+            ran = random.randrange(len(self.possible_cards))
+            self.use_card(ran)
+        else:
+            self.get_card()
+
+        if len(self.hand) == 1:
+            self.press_uno()
