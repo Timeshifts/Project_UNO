19828661da4f569b86cdeb9455ec389990190fd5
nodb <scnoh02@naver.com>
2023-03-30 18:40:33 +0900

    feat: single, single_lobby

--- src/constant.py
+++ src/constant.py
@@ -1,10 +1,17 @@
 import pygame
 from pathlib import Path
 
-RESOURCE_PATH = Path.cwd() / 'resources'
+RESOURCE_PATH = Path.cwd() / "resources"
 
 EVENT_QUIT_GAME = pygame.event.custom_type()
+EVENT_MAIN = pygame.event.custom_type()
+EVENT_START_LOBBY = pygame.event.custom_type()
 EVENT_START_SINGLE = pygame.event.custom_type()
 EVENT_OPEN_OPTION = pygame.event.custom_type()
 EVENT_OPTION_CHANGED = pygame.event.custom_type()
+EVENT_CLOSE_OPTION = pygame.event.custom_type()
 EVENT_START_MENU = pygame.event.custom_type()
+EVENT_START_STORY = pygame.event.custom_type()
+EVENT_PLAY_SE = pygame.event.custom_type()
+EVENT_OPEN_RENAME = pygame.event.custom_type()
+EVENT_CLOSE_RENAME = pygame.event.custom_type()
