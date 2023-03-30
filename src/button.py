8e928346ad6436d527268e6c394040777718d0b5
nodb <scnoh02@naver.com>
2023-03-30 18:40:01 +0900

    feat: 이미지 및 텍스트 변경 클래스

--- src/button.py
+++ src/button.py
@@ -1,6 +1,9 @@
 class Button:
-    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+    def __init__(
+        self, image, hovering_image, pos, text_input, font, base_color, hovering_color
+    ):
         self.image = image
+        self.hovering_image = hovering_image
         self.x_pos = pos[0]
         self.y_pos = pos[1]
         self.font = font
@@ -9,6 +12,8 @@ class Button:
         self.text = self.font.render(self.text_input, True, self.base_color)
         if self.image is None:
             self.image = self.text
+        if self.hovering_image is None:
+            self.hovering_image = self.text
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
@@ -24,11 +29,27 @@ class Button:
             return True
         return False
 
-    def changeColor(self, position, screen):
-        if position[0] in range(self.rect.left, self.rect.right) and position[
-            1
-        ] in range(self.rect.top, self.rect.bottom):
+    # def changeColor(self, position, screen):
+    #     if position[0] in range(self.rect.left, self.rect.right) and position[
+    #         1
+    #     ] in range(self.rect.top, self.rect.bottom):
+    #         self.text = self.font.render(self.text_input, True, self.hovering_color)
+    #         screen.blit(self.hovering_image, self.rect)
+    #     else:
+    #         self.text = self.font.render(self.text_input, True, self.base_color)
+
+    def forceChangeColor(self, state, screen):
+        if state:
             self.text = self.font.render(self.text_input, True, self.hovering_color)
-            screen.blit(self.image, self.rect)
+            screen.blit(self.hovering_image, self.rect)
         else:
             self.text = self.font.render(self.text_input, True, self.base_color)
+            screen.blit(self.image, self.rect)
+
+    def ChangeImage(self, change_image):
+        self.image = change_image
+
+    def ChangeText(self, change_text, change_base_color, change_hovering_color):
+        self.text_input = change_text
+        self.base_color, self.hovering_color = change_base_color, change_hovering_color
+        self.text = self.font.render(self.text_input, True, self.base_color)
