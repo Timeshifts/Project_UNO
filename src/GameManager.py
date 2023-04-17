f0075e3fafcb3e3642f8b6256fbb09f23b36da3b
LAPTOP-H19VRHGA\user <hyjin1117@gmail.com>
2023-04-17 19:10:34 +0900

    fix : turn_timer_integer 강제 초기화

--- src/GameManager.py
+++ src/GameManager.py
@@ -149,7 +149,9 @@ class GameManager:
         self.players[self.turn].attacked_int = 0
         self.turn_timer_end = True
         
-        pygame.time.wait(1500)
+        pygame.time.wait(1200)
+        
+        self.turn_timer_integer = 15
         
         # 현재 핸드가 0인지 판별
         if len(self.players[self.turn].hand) == 0:
